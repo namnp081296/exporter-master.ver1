1191bf1627839578ada83fa4dd639c67fc5599f8
Nguyen Phuong Nam <namnp081296@gmail.com>
2019-06-24 21:55:47 +0700

    my first commit

---
+++ yaml_handler/parse_yml.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+# How to use?
+# /bin/bash parse_yml.sh -> list all parameters
+# /bin/bash parse_yml.sh "mongo" -> list "mongo" parameter
+
+FILE="/root/exporter/yaml_handler/exporter.yml"
+
+if [[ -z $1 ]]; then
+	while read -r line
+	do
+		if [[ $line == *"param:"* ]]; then
+			echo $line | awk -F '"' '{print $2}' | awk -F '"' '{print $1}'
+		fi
+	done < $FILE
+else
+	QUERY="$1"
+	EXE=0
+	while read -r line
+	do
+		if [[ $EXE == 1 ]]; then
+			echo $line | awk -F '"' '{print $2}' | awk -F '"' '{print $1}'
+			EXE=0
+		fi
+		if [[ $line == *"$QUERY"* ]]; then
+			EXE=1
+		fi
+	done < $FILE
+fi

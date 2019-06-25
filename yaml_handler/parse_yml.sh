eb7a8daef3c22ef613cf674f21c5c7e73672874d
Nguyen Phuong Nam <namnp081296@gmail.com>
2019-06-25 10:01:51 +0700

    modified parse_yml.sh

--- yaml_handler/parse_yml.sh
+++ yaml_handler/parse_yml.sh
@@ -5,7 +5,8 @@
 # /bin/bash parse_yml.sh -> list all parameters
 # /bin/bash parse_yml.sh "mongo" -> list "mongo" parameter
 
-FILE="/root/exporter/yaml_handler/exporter.yml"
+CUR_DIR=`pwd`
+FILE="$CUR_DIR/yaml_handler/exporter.yml"
 
 if [[ -z $1 ]]; then
 	while read -r line

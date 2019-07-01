ee3b59743b7434c2442a422c68826f4c40e24c43
Nguyen Phuong Nam <namnp081296@gmail.com>
2019-07-01 16:41:08 +0700

    Edit main.sh code

--- main.sh
+++ main.sh
@@ -7,13 +7,7 @@ sudo mkdir /etc/prometheus
 sudo mv ~/exporter /etc/prometheus/exporter-master
 
 # Set HOMEPATH
-HOME_PATH=/etc/prometheus/exporter-master
-
-# Create folder log and run for user:prometheus
-# sudo mkdir -p /var/log/prometheus/
-# sudo mkdir -p /var/run/prometheus/
-# sudo chown -R prometheus:prometheus /var/log/prometheus
-# sudo chown -R prometheus:prometheus /var/run/prometheus
+HOME_PATH="/etc/prometheus/exporter-master"
 
 # Before we start running exporter. We should check existing port in server
 newport=0
@@ -39,7 +33,7 @@ function check_user_exist() {
 function check_file_exist() {
   for expter in "${arr_port[@]}"
   do
-    FILE=/usr/local/bin/exporter_${expter}
+    FILE="/usr/local/bin/exporter_${expter}"
     if [[ ! -e "$FILE" ]]
     then       
         #echo "file exporter_${expter} not exist"
@@ -55,10 +49,6 @@ function check_file_exist() {
 # Function Random port
 function random_service_port() {
    shuf -i 11020-11050 -n 1
-}
-
-function random_unused_port() {
-   shuf -i 1-100 -n 1
 }
 
 function get_version_centos() {
@@ -77,12 +67,14 @@ EOF"
 
 for expter in "${arr_port[@]}"
     do
-       PROGNAME=exporter_${expter}
-       PROG=/usr/local/bin/$PROGNAME
-       USER=prometheus
-       LOGFILE=/var/log/$USER/$PROGNAME.log
-       PIDFILE=/var/run/$USER/$PROGNAME.pid
-       LOCKFILE=/var/lock/subsys/$PROGNAME
+       PROGNAME="exporter_${expter}"
+       PROG="/usr/local/bin/${PROGNAME}"
+       USER="prometheus"
+       sudo touch /var/log/$USER/$PROGNAME.log
+       LOGFILE="/var/log/${USER}/${PROGNAME}.log"
+       sudo touch /var/run/$USER/$PROGNAME.pid
+       PIDFILE="/var/run/${USER}/${PROGNAME}.pid"
+       LOCKFILE="/var/lock/subsys/${PROGNAME}"
        RETVAL=0
        
        if [[ ${expter} == "merger" ]] ; then
@@ -102,7 +94,7 @@ PIDFILE=${PIDFILE}
 
 start() {
     echo -n "\"" Starting ${PROGNAME} "\"":
-    daemon --user ${USER} --pidfile="\""${PIDFILE}"\"" "\""${PROG} `/bin/bash ${HOME_PATH}/yaml_handler/parse_yml.sh ${expter}`$merger_port &>${LOGFILE} &"\""
+    daemon --user ${USER} --pidfile="\""${PIDFILE}"\"" "\""${PROG} `/bin/bash ${HOME_PATH}/yaml_handler/parse_yml.sh ${expter}`${merger_port} &>${LOGFILE} &"\""
     RETVAL=${RETVAL}
     echo
     [ ${RETVAL} -eq 0 ] && sudo touch ${LOCKFILE}
@@ -136,11 +128,60 @@ case "\""\$1"\"" in
     ;;
 esac
 EOF"
+       elif [[ ${expter} == "node" ]] ; then
+         node_port=11020
+         sudo bash -c "cat << 'EOF' > /etc/rc.d/init.d/exporter_${expter}
+#!/bin/bash
 
-#        cat << ADDTEXT | sudo tee -a /etc/merger.yaml
-# #${expter}
-# - url: http://localhost:$merger_port/metrics
-# ADDTEXT
+# Source function library.
+. /etc/rc.d/init.d/functions
+
+PROGNAME=${PROGNAME}
+PROG=${PROG}
+USER=${USER}
+LOGFILE=${LOGFILE}
+PIDFILE=${PIDFILE}
+
+start() {
+    echo -n "\"" Starting ${PROGNAME} "\"":
+    daemon --user ${USER} --pidfile="\""${PIDFILE}"\"" "\""${PROG} `/bin/bash ${HOME_PATH}/yaml_handler/parse_yml.sh ${expter}`${node_port} &>${LOGFILE} &"\""
+    RETVAL=${RETVAL}
+    echo
+    [ ${RETVAL} -eq 0 ] && sudo touch ${LOCKFILE}
+    echo
+}
+
+stop() {
+    echo -n "\"" Shutting down ${PROGNAME}: "\""
+    killproc ${PROGNAME}
+    rm -f ${LOCKFILE}
+    echo
+}
+
+case "\""\$1"\"" in
+    start)
+    start
+    ;;
+    stop)
+    stop
+    ;;
+    status)
+    status ${PROGNAME}
+    ;;
+    restart)
+    stop
+    start
+    ;;
+    *)
+        echo  "\""Usage: service exporter_${expter} {start|stop|status|reload|restart} "\""
+        exit 1
+    ;;
+esac
+EOF"
+       cat << ADDTEXT | sudo tee -a /etc/merger.yaml
+#${expter}
+- url: http://localhost:$node_port/metrics
+ADDTEXT
 
        else
          sudo netstat -lntp | grep $ser  > /dev/null
@@ -161,7 +202,7 @@ PIDFILE=${PIDFILE}
 
 start() {
     echo -n "\"" Starting ${PROGNAME} "\"": 
-    daemon --user ${USER} --pidfile="\""${PIDFILE}"\"" "\""${PROG} `/bin/bash ${HOME_PATH}/yaml_handler/parse_yml.sh ${expter}`$service_port &>${LOGFILE} &"\""
+    daemon --user ${USER} --pidfile="\""${PIDFILE}"\"" "\""${PROG} `/bin/bash ${HOME_PATH}/yaml_handler/parse_yml.sh ${expter}`${service_port} &>${LOGFILE} &"\""
     RETVAL=${RETVAL}
     echo
     [ ${RETVAL} -eq 0 ] && sudo touch ${LOCKFILE}
@@ -201,15 +242,11 @@ EOF"
 - url: http://localhost:$service_port/metrics
 ADDTEXT
     else
-        #echo "Port is in used"
-        rd=$(random_unused_port)
-        #echo "Random port is $rd"
-        newport=$(( service_port + rd ))
-        #echo "Your new port is: $newport "
         while true;
         do
+           newport=$(random_service_port)
            sudo netstat -lntp | grep $newport > /dev/null
-           if [ $? == 1 ] ; then
+           if [[ $? == 1 && $newport != $node_port && $newport != $merger_port ]] ; then
              sudo bash -c "cat << 'EOF' > /etc/rc.d/init.d/exporter_${expter}
 #!/bin/bash
 
@@ -224,7 +261,7 @@ PIDFILE=${PIDFILE}
 
 start() {
     echo -n "\"" Starting ${PROGNAME} "\"": 
-    daemon --user ${USER} --pidfile="\""${PIDFILE}"\"" "\""${PROG} `/bin/bash ${HOME_PATH}/yaml_handler/parse_yml.sh ${expter}`$newport &>${LOGFILE} &"\""
+    daemon --user ${USER} --pidfile="\""${PIDFILE}"\"" "\""${PROG} `/bin/bash ${HOME_PATH}/yaml_handler/parse_yml.sh ${expter}`${newport} &>${LOGFILE} &"\""
     RETVAL=${RETVAL}
     echo
     [ ${RETVAL} -eq 0 ] && sudo touch ${LOCKFILE}
@@ -268,10 +305,20 @@ ADDTEXT
         done
     fi
  fi
- sudo chown -R prometheus:prometheus /etc/rc.d/init.d/exporter_*
- sudo chmod 755 /etc/rc.d/init.d/exporter_*
- sudo service exporter_${expter} start
- sudo service exporter_${expter} status
+ SERVICE="exporter_${expter}"
+ sudo chown -R prometheus:prometheus /etc/rc.d/init.d/$SERVICE
+ sudo chmod 755 /etc/rc.d/init.d/$SERVICE
+
+ pidof -x $SERVICE > /dev/null
+ if [[ $? == 1 ]] ; then
+      #echo "$SERVICE is not running"
+      sudo service $SERVICE start
+      sudo service $SERVICE status
+ else
+      #echo "$SERVICE running"
+      sudo service $SERVICE restart
+      sudo service $SERVICE status
+ fi
 
 done
 }
@@ -300,16 +347,34 @@ After=network-online.target
 User=prometheus
 Group=prometheus
 Type=simple
-ExecStart=/usr/local/bin/exporter_${expter} `/bin/bash $HOME_PATH/yaml_handler/parse_yml.sh ${expter}`$merger_port
+ExecStart=/usr/local/bin/exporter_${expter} `/bin/bash ${HOME_PATH}/yaml_handler/parse_yml.sh ${expter}`${merger_port}
 
 [Install]
 WantedBy=multi-user.target
 EOF"
 
-#        cat << ADDTEXT | sudo tee -a /etc/merger.yaml
-# #${expter}
-# - url: http://localhost:$merger_port/metrics
-# ADDTEXT
+    elif [[ ${expter} == "node" ]] ; then
+       node_port=11020
+       sudo bash -c "cat << 'EOF' > /etc/systemd/system/exporter_${expter}.service
+[Unit]
+Description= exporter ${expter}
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=prometheus
+Group=prometheus
+Type=simple
+ExecStart=/usr/local/bin/exporter_${expter} `/bin/bash ${HOME_PATH}/yaml_handler/parse_yml.sh ${expter}`${node_port}
+
+[Install]
+WantedBy=multi-user.target
+EOF"
+
+        cat << ADDTEXT | sudo tee -a /etc/merger.yaml
+#${expter}
+- url: http://localhost:$node_port/metrics
+ADDTEXT
 
     else
        sudo netstat -lntp | grep $ser  > /dev/null
@@ -326,7 +391,7 @@ After=network-online.target
 User=prometheus
 Group=prometheus
 Type=simple
-ExecStart=/usr/local/bin/exporter_${expter} `/bin/bash $HOME_PATH/yaml_handler/parse_yml.sh ${expter}`$service_port
+ExecStart=/usr/local/bin/exporter_${expter} `/bin/bash ${HOME_PATH}/yaml_handler/parse_yml.sh ${expter}`${service_port}
 
 [Install]
 WantedBy=multi-user.target
@@ -339,15 +404,11 @@ EOF"
 ADDTEXT
 
        else
-        #echo "Port is in used"
-        rd=$(random_unused_port)
-        #echo "Random port is $rd"
-        newport=$(( service_port + rd ))
-        #echo "Your new port is: $newport "
         while true;
         do
+           newport=$(random_service_port)
            sudo netstat -lntp | grep $newport > /dev/null
-           if [ $? == 1 ] ; then
+           if [[ $? == 1 && $newport != $node_port && $newport != $merger_port ]] ; then
              sudo bash -c "cat << 'EOF' >  /etc/systemd/system/exporter_${expter}.service
 [Unit]
 Description= exporter ${expter}
@@ -358,7 +419,7 @@ After=network-online.target
 User=prometheus
 Group=prometheus
 Type=simple
-ExecStart=/usr/local/bin/exporter_${expter} `/bin/bash $HOME_PATH/yaml_handler/parse_yml.sh ${expter}`$newport
+ExecStart=/usr/local/bin/exporter_${expter} `/bin/bash ${HOME_PATH}/yaml_handler/parse_yml.sh ${expter}`${newport}
 
 [Install]
 WantedBy=multi-user.target
@@ -369,17 +430,24 @@ EOF"
 - url: http://localhost:$newport/metrics
 ADDTEXT
 
-             #echo "Port $newport is validable"
              break;
            fi
         done
       fi
    fi
 # Reload daemon then start, enable and check status.
+SERVICE="exporter_${expter}.service"
 sudo systemctl daemon-reload
-sudo systemctl start exporter_${expter}.service
-sudo systemctl enable exporter_${expter}.service
-sudo systemctl status exporter_${expter}.service
+pidof -x $SERVICE > /dev/null
+ if [[ $? == 1 ]] ; then
+      #echo "$SERVICE is not running"
+      sudo systemctl start $SERVICE
+      sudo systemctl enable $SERVICE
+      sudo systemctl status $SERVICE
+ else
+      #echo "$SERVICE running"
+      sudo systemctl restart $SERVICE
+      sudo systemctl status $SERVICE
 done
 
 }
